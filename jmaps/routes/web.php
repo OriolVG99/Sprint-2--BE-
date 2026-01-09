9d9ea03786011495b4cd5bbec03d1ecda8f09aaf
oriol <ovidalg@fp.insjoaquimmir.cat>
2026-01-09 17:08:46 +0100

    Abans de canviar de repositori, apartat fins abans de 5.2

--- jmaps/routes/web.php
+++ jmaps/routes/web.php
@@ -3,6 +3,20 @@
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\ProfileController;
 
 
 Route::get('/', [SiteController::class, 'home']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
